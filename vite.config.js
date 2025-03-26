af0997da1c64918bd969850ac80ec344ab4086e2
naverstay <naverstay.me@gmail.com>
2025-03-26 15:19:36 +0100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/gpg-json-verifier/',
+  build: {
+    rollupOptions: {
+      external: [], // Добавь пустой массив, чтобы Vite не пытался исключить openpgp
+    },
+  },
+  optimizeDeps: {
+    include: ['openpgp'], // Явно укажем openpgp как зависимость
+  },
 })
